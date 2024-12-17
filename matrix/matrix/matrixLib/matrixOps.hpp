3422071c9c110f5aee460ef8b57b19dfe625c005
SanaePRJ <sanaeprj@gmail.com>
2024-12-17 17:12:42 +0900

    記法を統一させます

--- matrix/matrix/matrixLib/matrixOps.hpp
+++ matrix/matrix/matrixLib/matrixOps.hpp
@@ -1,107 +1,81 @@
 #ifndef MATRIXCPP_MATRIXOP_HPP
 #define MATRIXCPP_MATRIXOP_HPP
 
+
 #include "matrix.h"
 
-// 代入演算子 =
-//	matrix =
-//	{
-//		{1, 0, 0, 0},
-//		{0, 1, 0, 0},
-//		{0, 0, 1, 0},
-//		{0, 0, 0, 1}
-//	}
+
+// 代入演算子 = 初期化リスト用
 template<typename Type>
-Matrix<Type>& Matrix<Type>::operator=
-(
-	const MatrixInitType<Type>& init
-)
+Matrix<Type>& Matrix<Type>::operator=(const MatrixInitType<Type>& init)
 {
-	this->matrix_.clear();
-	this->matrix_ = {init.begin(), init.end()};
+    this->matrix_.clear();
+    this->matrix_.assign(init.begin(), init.end());
 
-	// バリデート
+    // 必要なバリデートをここに追加
+    // validateMatrix_(this->matrix_);
 
-	return *this;
+    return *this;
 }
 
 // コピー代入演算子 =
 template<typename Type>
-Matrix<Type>& Matrix<Type>::operator=
-(
-	const Matrix<Type>& other
-)
+Matrix<Type>& Matrix<Type>::operator=(const Matrix<Type>& other)
 {
-	if (this != &other) {
-		this->matrix_.clear();
-		copyMatrix_(this->matrix_, other.matrix_);
-	}
+    if (this != &other)
+        copyMatrix_(this->matrix_, other.matrix_);
 
-	return *this;
+    return *this;
 }
 
 // ムーブ代入演算子 =
 template<typename Type>
-Matrix<Type>& Matrix<Type>::operator=
-(
-	Matrix<Type>&& other
-)
+Matrix<Type>& Matrix<Type>::operator=(Matrix<Type>&& other)
 {
-	if (this != &other) {
-		this->matrix_.clear();
-		this->matrix_ = std::move(other.matrix_);
-	}
-
-	return *this;
+    if (this != &other)
+        this->matrix_ = std::move(other.matrix_);
+    
+    return *this;
 }
 
 // 代入演算子 << 初期化リスト用
 template<typename Type>
-Matrix<Type>& Matrix<Type>::operator<<
-(
-	const MatrixInitType<Type>& init
-)
+Matrix<Type>& Matrix<Type>::operator<<(const MatrixInitType<Type>& init)
 {
-	this->matrix_.clear();
-	this->matrix_ = init;
+    this->matrix_.clear();
+    this->matrix_.assign(init.begin(), init.end());
 
-	return *this;
+    // 必要なバリデートをここに追加
+    // validateMatrix_(this->matrix_);
+
+    return *this;
 }
 
 // 代入演算子 << コピー
 template<typename Type>
-Matrix<Type>& Matrix<Type>::operator<<
-(
-	const Matrix<Type>& other
-)
+Matrix<Type>& Matrix<Type>::operator<<(const Matrix<Type>& other)
 {
-	this->matrix_.clear();
-	this->matrix_ = other;
-
-	return *this;
+    if (this != &other)
+        this->matrix_ = other.matrix_;
+    
+    return *this;
 }
 
 // ムーブ代入演算子 <<
 template<typename Type>
-Matrix<Type>& Matrix<Type>::operator<<
-(
-	Matrix<Type>&& other
-)
+Matrix<Type>& Matrix<Type>::operator<<(Matrix<Type>&& other)
 {
-	this->matrix_.clear();
-	this->matrix_ = std::move(other.matrix_);
-
-	return *this;
+    if (this != &other)
+        this->matrix_ = std::move(other.matrix_);
+    
+    return *this;
 }
 
-// 行アクセス
+// 行アクセスオペレーター
 template<typename Type>
-typename Matrix<Type>::RowType<Type>& Matrix<Type>::operator[]
-(
-	size_t index
-)
+typename Matrix<Type>::RowType<Type>& Matrix<Type>::operator[](size_t index)
 {
-	return this->matrix_[index];
+    return this->matrix_[index];
 }
 
 
